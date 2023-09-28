6e4c3d445208c64518904d7c12edadbf77e77bbf
MOISES <moisessalinas19@augustana.edu>
2023-09-28 09:32:57 -0500

    Now with dancing chicken

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -3,5 +3,8 @@ package edu.augustana;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello bird world!");
+
+        Chicken chickenDance = new Chicken();
+        chickenDance.dance();
     }
 }
